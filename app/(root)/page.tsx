1a599c35e0e707d6d3e2b4b718c11f7e9136e334
GuusWolters <67540780+GuusWolters@users.noreply.github.com>
2024-08-11 13:27:48 +0200

    Implement the complete layour

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import HeaderBox from "@/components/ui/HeaderBox";
 
 function Home() {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Guus",
+    lastName: "Hiddink",
+    email: "guus.wolters@outlook.com",
+  };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcom"
+            subtext="Access and manage your account and transactions efficiently."
+            user={loggedIn?.firstName || "Guest"}
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
+    </section>
+  );
 }
 
 export default Home;
